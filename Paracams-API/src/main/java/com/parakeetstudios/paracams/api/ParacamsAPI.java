71029bb51a346b774d6a8bad06b5b8909664cbe2
SheeshSpace <cammy.sheesh@gmail.com>
2023-08-26 20:42:30 +1200

    Super basic impl f or API hook/instance class

--- Paracams-API/src/main/java/com/parakeetstudios/paracams/api/ParacamsAPI.java
+++ Paracams-API/src/main/java/com/parakeetstudios/paracams/api/ParacamsAPI.java
@@ -1,4 +1,39 @@
 package com.parakeetstudios.paracams.api;
 
-public class ParacamsAPI {
+import com.parakeetstudios.paracams.api.registers.CameraRegistry;
+
+public final class ParacamsAPI {
+
+    private CameraRegistry cameraRegistry;
+    private final Object lock = new Object();
+
+    private ParacamsAPI(){};
+
+    private static final class InstanceHolder {
+        private static final ParacamsAPI instance = new ParacamsAPI();
+    }
+
+    public static ParacamsAPI getInstance() {
+        return InstanceHolder.instance;
+    }
+
+    public void onEnable(CameraRegistry registry) {
+        synchronized (lock) {
+            if (this.cameraRegistry == null) {
+                this.cameraRegistry = registry;
+            } else {
+                throw new IllegalStateException("ParacamsAPI has already been enabled");
+            }
+        }
+    }
+
+    public synchronized void onDisable() {
+        synchronized (lock) {
+            if (this.cameraRegistry != null) {
+                this.cameraRegistry.clear();
+                this.cameraRegistry = null;
+            }
+        }
+    }
+
 }
